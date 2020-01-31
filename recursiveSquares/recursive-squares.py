756c311f33b0f3a0c64dba382a8b1bc3c9f86700
Chukwujama Osinachi Victor <vicraph7@gmail.com>
2020-01-31 21:36:39 +0100

    Add the turtle implementation of recursive squares

---
+++ recursiveSquares/recursive-squares.py
@@ -0,0 +1,31 @@
+import turtle as t
+t.speed(0)
+
+
+
+def rect(w, h):
+    for i in range(2):
+        t.fd(w)
+        t.lt(90)
+        t.fd(h)
+        t.lt(90)
+
+def recursiveSquares(width, height, padding):
+    dummyHeight = height//8
+
+    for i in range(dummyHeight):
+        rect(width, height)
+        
+        t.pu()
+        t.fd(padding)
+        t.lt(90)
+        t.fd(padding)
+        t.rt(90)
+        t.pd()
+        
+        width -= padding*2
+        height -= padding*2
+        
+t.ht()
+
+recursiveSquares(200, 100, 12)
